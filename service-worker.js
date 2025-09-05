56bc60f2691726015cf7b3d5ef716c400417da1a
amanda-design-creator <amandaamarasekera@gmail.com>
2025-09-05 21:40:12 +0530

    Add files via upload
    Initial upload of Family Grocery List app

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// service-worker.js
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
